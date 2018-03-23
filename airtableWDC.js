64c0a6ac9969312c4e3db73474e76c287579254b
stevenc-slalom <stevenc@slalom.com>
2018-03-23 14:31:46 -0400

    Update airtableWDC.js

--- airtableWDC.js
+++ airtableWDC.js
@@ -10,8 +10,8 @@
         }];
 
         var tableSchema = {
-            id: "earthquakeFeed",
-            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+            id: "airTableFeed",
+            alias: "Airtable for Work To-Do List",
             columns: cols
         };
 
@@ -20,17 +20,14 @@
 
     // Download the data
     myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
+        $.getJSON("https://api.airtable.com/v0/appsfhvN4p118JawU/List?api_key=keyt6RWODoHzDSTF1", function(resp) {
             var feat = resp.features,
                 tableData = [];
 
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-                    "id": feat[i].id,
-                    "mag": feat[i].properties.mag,
-                    "title": feat[i].properties.title,
-                    "location": feat[i].geometry
+                    "id": feat[i].records.id
                 });
             }
 
@@ -44,7 +41,7 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+            tableau.connectionName = "Airtable Data"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
         });
     });
