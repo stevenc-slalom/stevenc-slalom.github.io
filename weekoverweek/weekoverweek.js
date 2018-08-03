b7bba087aa5feab95fed81011a122d6ecc7f60e2
stevenc-slalom <stevenc@slalom.com>
2018-08-03 10:15:50 -0400

    Update weekoverweek.js

--- weekoverweek/weekoverweek.js
+++ weekoverweek/weekoverweek.js
@@ -2,10 +2,10 @@ window.onload=function(){
     alert('Hello World!');
     
     $date = moment().startOf('week')
-    $p1start = $date.add(-7,'day')
-    $p1end = $date.add(-1,'day')
-    $p2start = $date.add(-14,'day')
-    $p2end = $date.add(-8,'day')
+    $p1start = $date.clone().add(-7,'day')
+    $p1end = $date.clone().add(-1,'day')
+    $p2start = $date.clone().add(-14,'day')
+    $p2end = $date.clone().add(-8,'day')
     alert($p1start);
     alert($p1end);
     alert($p2start);
